29799bc0cef8dffa5340dbb5dae58fad35a3d550
Falk Werner <falk.werner@gmx.net>
2025-01-18 21:52:41 +0100

    add test for insecure connetsions, post data, multipart form data and custom http headers
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

--- test-utils/test-server/src/main.rs
+++ test-utils/test-server/src/main.rs
@@ -1,19 +1,78 @@
 use axum::{
+    extract::Multipart,
+    extract::Request,
+    http::header::USER_AGENT,
     routing::get,
+    routing::post,
     Router,
 };
 
-use std::{thread, time::Duration};
+use axum_server::tls_rustls::RustlsConfig;
+use std::{thread, time::Duration, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/slow_answer", get(slow_answer));
+    let app = Router::new()
+        .route("/", get(welcome))
+        .route("/slow_answer", get(slow_answer))
+        .route("/echo_data", post(echo_data))
+        .route("/echo_form", post(echo_form))
+        .route("/user_agent", get(get_user_agent))
+        ;
 
-    let listener = tokio::net::TcpListener::bind("localhost:9000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+//    let listener = tokio::net::TcpListener::bind("localhost:9000").await.unwrap();
+//    axum::serve(listener, app).await.unwrap();
+
+
+    // configure certificate and private key used by https
+    let key = include_bytes!("key.pem").to_vec();
+    let cert = include_bytes!("cert.pem").to_vec();
+    let config = RustlsConfig::from_pem(cert, key)
+        .await
+        .unwrap();
+
+    // run https server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 9000));
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn welcome() -> &'static str {
+    "Welcome!"
 }
 
 async fn slow_answer() -> &'static str {
     thread::sleep(Duration::from_secs(30));
-    "Hello, World!"
+    "42"
+}
+
+async fn echo_data(body:String) -> String {
+    body
+}
+
+async fn echo_form(mut multipart: Multipart) -> String {
+    let mut result = String::new();
+
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let data = String::from(std::str::from_utf8(&field.bytes().await.unwrap()).unwrap());
+
+        result.push_str(&format!("{} = {};", name, data));
+    }
+
+    result
+}
+
+async fn get_user_agent(request: Request) -> String {
+    let user_agent_header = request.headers().get(USER_AGENT);
+    let user_agent = user_agent_header.and_then(|value| value.to_str().ok());
+
+    if let Some(user_agent) = user_agent {
+        String::from(user_agent)
+    }
+    else {
+        String::from("unknonwn")
+    }
 }
--- test/test-server.bats
+++ test/test-server.bats
@@ -18,9 +18,36 @@ teardown() {
     kill $CHILD
 }
 
+@test "connect insecure" {
+    data=$($FETCH -K https://localhost:9000/)
+    [[ "$data" == "Welcome!" ]]
+}
 
-@test "download fails due to timeout (--max-time)" {
-    if $FETCH http://localhost:9000/slow_answer --max-time 5; then
+@test "connection fails on insecure servers" {
+    if $FETCH https://localhost:9000/ ; then
         false
     fi
+}
+
+@test "download fails due to timeout (--max-time)" {
+    if $FETCH -K https://localhost:9000/slow_answer --max-time 5; then
+        false
+    fi
+}
+
+@test "post data" {
+    data=$($FETCH -K https://localhost:9000/echo_data -d foobar)
+    [[ "$data" == "foobar" ]]
+}
+
+@test "post form data" {
+    data=$($FETCH -K https://localhost:9000/echo_form -F name=Bob -F type=cat)
+    echo "$data"
+    [[ "$data" == "name = Bob;type = cat;" ]]
+}
+
+@test "add http header" {
+    data=$($FETCH -K https://localhost:9000/user_agent -H "User-Agent: spacebox")
+    echo "$data"
+    [[ "$data" == "spacebox" ]]
 }
